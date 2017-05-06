642aa1ddaefb4395cb9986c2d7cd319f192a99e4
Martyna Graca <martynagraca@pc57.home>
2017-05-06 22:08:29 +0200

    Dodanie nazwy grupy w navbar

--- js/app.js
+++ js/app.js
@@ -268,14 +268,22 @@ MyApp.controller("groupsController", function ($scope, $uibModal, $cookies, $htt
 
 });
 
-MyApp.controller("navbarController", function ($scope, $cookies, $location) {
-    $scope.title = "navbarController";
-    var username = $cookies.get('login');
+MyApp.controller("navbarController", function ($scope, $cookies, $location, $http) {
+
+    var parameterGroupName = JSON.stringify({type:"groupName", id_grupy:$cookies.get('id_grupy')});
+
+	$http.post("./js/getGroupName.php", parameterGroupName)
+	    .then(function (response) {
+	   		$cookies.put('nazwa_grupy', response.data.records[0].login);
+		});
     
-    $scope.username = username;
+    $scope.username = $cookies.get('login');
+    $scope.groupName = $cookies.get('nazwa_grupy');
+
     $scope.logout = function() {
     	$cookies.put('login', "");
     	$cookies.put('id_grupy', "");
+    	$cookies.put('nazwa_grupy', "");
     	$location.path('/login');
     //	$route.reload();
     }
--- js/getGroupName.php
+++ js/getGroupName.php
@@ -1,5 +1,4 @@
 <?php
-
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
@@ -7,20 +6,16 @@ $data = json_decode(file_get_contents("php://input"));
 
 $conn = new mysqli("10.254.94.2", "s173529", "Parkowa3", "s173529");
 
-$result = $conn->query("SELECT id_grupy, login, haslo FROM Uzytkownicy WHERE login = '$data->username' AND haslo = '$data->password'");
+$result = $conn->query("SELECT login FROM Grupy WHERE id_grupy = $data->id_grupy");
 
 $outp = "";
-
-
 while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
     if ($outp != "") {$outp .= ",";}
-    $outp .= '{"login":"'  . $rs["login"] . '",';
-    $outp .= '"haslo":"'. $rs["haslo"]     . '",';
-    $outp .= '"id_grupy":"'. $rs["id_grupy"]     . '"}';
+    $outp .= '{"login":"'  . $rs["login"] . '"}';
 }
 $outp ='{"records":['.$outp.']}';
 $conn->close();
 
 echo($outp); 
-//echo($data);
+
 ?>
